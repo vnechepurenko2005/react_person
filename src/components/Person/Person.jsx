b4b96b94674b5579fc80d90cbe96ea38645e89a7
Vladyslav Nechepurenko <fablevlados@gmail.com>
2025-09-06 01:37:49 +0300

    add logic for partnerlabel and edit solution for person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,16 +1,18 @@
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">My name is {person.name}</h2>
+export const Person = ({ person }) => {
+  const partnerLabel = person.sex === 'm' ? 'wife' : 'husband';
 
-    {person.age > 0 && <p className="Person__age">I am {person.age}</p>}
-    {person.sex === 'm' && person.isMarried && (
-      <p className="Person__partner">{person.partnerName} is my wife</p>
-    )}
-    {person.sex === 'f' && person.isMarried && (
-      <p className="Person__partner">{person.partnerName} is my husband</p>
-    )}
-    {person.isMarried === false && (
-      <p className="Person__partner">I am not married</p>
-    )}
-  </section>
-);
+  return (
+    <section className="Person">
+      <h2 className="Person__name">My name is {person.name}</h2>
+
+      {person.age !== 0 && <p className="Person__age">I am {person.age}</p>}
+      {person.isMarried ? (
+        <p className="Person__partner">
+          {person.partnerName} is my {partnerLabel}
+        </p>
+      ) : (
+        <p className="Person__partner">I am not married</p>
+      )}
+    </section>
+  );
+};
