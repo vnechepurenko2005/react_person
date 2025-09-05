702b07b9096304dffaee64973db17ca39bc5ed84
Vladyslav Nechepurenko <fablevlados@gmail.com>
2025-09-06 01:43:20 +0300

    edit marital logic and age logic

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,12 +1,15 @@
 export const Person = ({ person }) => {
   const partnerLabel = person.sex === 'm' ? 'wife' : 'husband';
+  const maritalStatus = person.isMarried;
 
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
 
-      {person.age !== 0 && <p className="Person__age">I am {person.age}</p>}
-      {person.isMarried ? (
+      {person.age !== undefined && (
+        <p className="Person__age">I am {person.age}</p>
+      )}
+      {maritalStatus ? (
         <p className="Person__partner">
           {person.partnerName} is my {partnerLabel}
         </p>
